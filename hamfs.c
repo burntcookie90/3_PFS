0ad1f95654af32843a379e2a1d28733068fd330f
Vishnu Sreedharan Rajeevan <vrajeevan3@factor032.cc.gatech.edu>
2012-11-18 20:07:18 -0500

    more basic functions in hamfs

--- hamfs.c
+++ hamfs.c
@@ -6,14 +6,29 @@
 #include <errno.h>
 #include <fcntl.h>
 
+
+
+static int ham_getattr(const char *path, struct stat *stbuf){
+
+}
+
+static int ham_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
+
+}
+
+static int ham_open(const char *path, struct fuse_file_info *fi) {
+
+}
+
+static int ham_read(const char *path, char *buf, size_t size, off_t offset,struct fuse_file_info *fi){
+
+}
 static struct fuse_operations ham_oper= {
 	.getattr   = ham_getattr,
 	.readdir = ham_readdir,
 	.open   = ham_open,
 	.read   = ham_read,
 };
-  
-
 
 int main(int argc, char *argv[])
 {
