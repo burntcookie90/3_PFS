ab18ef3a345b945fd127f60ce820642373c74759
Vishnu Sreedharan Rajeevan <vrajeevan3@factor032.cc.gatech.edu>
2012-11-18 20:04:27 -0500

    basic junk in hamf

--- hamfs.c
+++ hamfs.c
@@ -1,7 +1,21 @@
  #define FUSE_USE_VERSION  26
    
-  #include <fuse.h>
-  #include <stdio.h>
-  #include <string.h>
-  #include <errno.h>
-  #include <fcntl.h>
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+
+static struct fuse_operations ham_oper= {
+	.getattr   = ham_getattr,
+	.readdir = ham_readdir,
+	.open   = ham_open,
+	.read   = ham_read,
+};
+  
+
+
+int main(int argc, char *argv[])
+{
+	return fuse_main(argc, argv, &ham_oper, NULL);
+}
